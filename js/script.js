6f27d3d0ad213d57a683ee3d850c104a42b459b6
ismael008rd <ismael008@gmail.com>
2024-02-10 19:06:08 -0300

    acho que não deu certo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.btn')
+const height = window.innerHeight - 50
+const width = window.innerWidth - 50
+
+button.addEventListener('mouseover', ()=>{
+    button.style.position ='absolute'
+    button.style.top= Math.random()*height+'px'
+    button.style.left= Math.random()*width+'px'
+})
